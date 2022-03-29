f91e7bd5088dfcab64341a2c7b276fb93ed6c1b0
brian <bmetten123@hotmail.com>
2022-03-29 10:34:07 -0500

    remove testing comment
    removed comment used for testing

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 echo Invoke-WebRequest -Uri https://raw.githubusercontent.com/briantehowenerer/Maintenance-Checkup/main/MC.ps1 -OutFile MC.ps1 > start.ps1
-echo #iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI" >> start.ps1
+echo iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI" >> start.ps1
 echo Start-process $PSScriptRoot\MC.ps1 >> start.ps1
 PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
